2927a5398e11996e30e34e36bd50db2125c5b9f7
Chase <CraigVella@gmail.com>
2022-08-01 20:52:07 -0400

    Added Triggers

--- include/VVeggies.h
+++ include/VVeggies.h
@@ -6,10 +6,14 @@
 #define __VVEGGIES__
 
 #define EE_ADDR_UPDATEFREQ  0x00
-#define EE_ADDR_MS1TRIG     0x02
-#define EE_ADDR_MS2TRIG     0x04
+#define EE_ADDR_ST0TRIG     0x02
+#define EE_ADDR_ST1TRIG     0x04
+#define EE_ADDR_TRIGEN      0x06
+#define EE_ADDR_MAXWT       0x07
+#define EE_ADDR_MAXOT       0x0B
+#define EE_ADDR_TRIGHT      0x0F
 
-#define VVSTATE_SIZE        10
+#define VVSTATE_SIZE        18
 
 class VVeggies {
     protected:
@@ -24,13 +28,23 @@ class VVeggies {
     struct VVState {
         time_t lastReading                = 0;
         bool waterState                   = false;
+        bool inOverride                   = false;
+        unsigned long timeWatering        = 0;
+        unsigned long maxWateringTime     = 0;
+        unsigned long maxOverrideTime     = 0;
+        unsigned long triggerHoldTime     = 0;
+        unsigned long triggerHeld         = 0;
         uint16_t moistureSensor[2]        = {};
         uint16_t moistureSensorTrigger[2] = {};
         uint16_t batteryReading           = 0;
         uint16_t updateFrequency          = 0;
+        bool triggersEnabled              = true;
     } state;
 
     unsigned long TIMER_lastTime = 0;
+    unsigned long TIMER_lastIndependantTime = 0;
+
+    void evaluateWater(unsigned long delta);
 
     public:
     VVeggies(std::function<void(void)> onStateUpdate);
@@ -38,11 +52,15 @@ class VVeggies {
     String getStateJson(void);
     void doTasks(void);
     void processCommand(const char * command, const char * arg);
-    void evaluateWater();
     void waterOverride(bool value);
     void turnOnWater(bool value);
     void setUpdateFrequency(uint16_t freq);
     void updateInternalStateFromEEPROM(void);
+    void setSoilTrigger(uint8_t number, uint16_t value);
+    void setTriggersEnabled(bool enabled);
+    void setMaxWateringTime(unsigned long maxWT);
+    void setMaxOverrideTime(unsigned long maxOT);
+    void setTriggerHoldTime(unsigned long trigHT);
 };
 
 #endif
--- src/VVeggies.cpp
+++ src/VVeggies.cpp
@@ -19,15 +19,23 @@ const VVeggies::VVState& VVeggies::getState() {
 }
 
 void VVeggies::doTasks() {
+    uint32_t delta = millis() - this->TIMER_lastIndependantTime;
+    this->evaluateWater(delta);
     if ((millis() - this->TIMER_lastTime) >= this->state.updateFrequency) {
-      this->evaluateWater();
+      this->_onStateUpdateCallback();
       this->TIMER_lastTime = millis();
     }
+    if (delta)
+        this->TIMER_lastIndependantTime = millis();
 }
 
 String VVeggies::getStateJson() {
     StaticJsonDocument<JSON_OBJECT_SIZE(VVSTATE_SIZE)> json;
     json["waterState"] = this->getState().waterState;
+    json["inOverride"] = this->getState().inOverride;
+    json["maxWateringTime"] = this->getState().maxWateringTime;
+    json["maxOverrideTime"] = this->getState().maxOverrideTime;
+    json["timeWatering"] = this->getState().timeWatering;
     json["dateTime"] = this->getState().lastReading;
     json["soil"][0]["value"] = this->getState().moistureSensor[0];
     json["soil"][0]["trigger"] = this->getState().moistureSensorTrigger[0];
@@ -35,6 +43,9 @@ String VVeggies::getStateJson() {
     json["soil"][1]["trigger"] = this->getState().moistureSensorTrigger[1];
     json["battery"] = this->getState().batteryReading;
     json["updateFrequency"] = this->getState().updateFrequency;
+    json["triggersEnabled"] = this->getState().triggersEnabled;
+    json["triggerHoldTime"] = this->getState().triggerHoldTime;
+    json["triggerHeld"] = this->getState().triggerHeld;
     
     unsigned long jsonSize = measureJson(json)+1;
     char * buff = (char *) malloc(jsonSize);
@@ -51,18 +62,78 @@ void VVeggies::processCommand(const char * command, const char * arg) {
       } else if (strcmp("waterOverride",command) == 0) {
         Serial.println("Water override recieved");
         this->waterOverride((arg[0] == '0') ? false : true);
+      } else if (strcmp("setSoilTrigger0",command) == 0) {
+        Serial.println(String("Setting Soil Trigger 0 to ") + arg);
+        this->setSoilTrigger(0,strtoul(arg,NULL,10));
+      } else if (strcmp("setSoilTrigger1",command) == 0) {
+        Serial.println(String("Setting Soil Trigger 1 to ") + arg);
+        this->setSoilTrigger(1,strtoul(arg,NULL,10));
+      } else if (strcmp("setTriggersEnabled",command) == 0) {
+        Serial.println(String("Setting Triggers enabled to ") + arg);
+        this->setTriggersEnabled((arg[0] == '0') ? false : true);
+      } else if (strcmp("setMaxWateringTime",command) == 0) {
+        Serial.println(String("Setting Max Watering Time to ") + arg);
+        this->setMaxWateringTime(strtoul(arg, NULL, 10));
+      } else if (strcmp("setMaxOverrideTime",command) == 0) {
+        Serial.println(String("Setting Max Override Time to ") + arg);
+        this->setMaxOverrideTime(strtoul(arg, NULL, 10));
+      } else if (strcmp("setTriggerHoldTime",command) == 0) {
+        Serial.println(String("Setting Trigger Hold Time to: ") + arg);
+        this->setTriggerHoldTime(strtoul(arg, NULL, 10));
       }
 }
 
-void VVeggies::evaluateWater() {
+void VVeggies::setTriggersEnabled(bool enabled) {
+    bool en = EEPROM.readBool(EE_ADDR_TRIGEN);
+    if (en != enabled) {
+        EEPROM.writeBool(EE_ADDR_TRIGEN, enabled);
+        EEPROM.commit();
+        this->state.triggersEnabled = enabled;
+        this->_onStateUpdateCallback();
+    }
+}
+
+void VVeggies::evaluateWater(unsigned long delta) {
     this->state.lastReading = time(NULL);
     this->state.moistureSensor[0] = analogRead(VVeggies::PIN_SoilSensor1);
     this->state.moistureSensor[1] = analogRead(VVeggies::PIN_SoilSensor2);
     this->state.batteryReading = analogRead(VVeggies::PIN_Battery);
-    this->_onStateUpdateCallback();
+    if (this->getState().waterState) {
+        this->state.timeWatering += delta;
+    } else {
+        this->state.timeWatering = 0;
+    }
+    if (this->getState().triggersEnabled && !this->getState().inOverride) {
+        if ((this->getState().moistureSensor[0] >= this->getState().moistureSensorTrigger[0]) ||
+            (this->getState().moistureSensor[1] >= this->getState().moistureSensorTrigger[1])) {
+            this->state.triggerHeld += delta;
+        } else {
+            this->state.triggerHeld = 0;
+        }
+        if ((this->getState().triggerHeld >= this->getState().triggerHoldTime) && 
+            (this->getState().timeWatering < this->getState().maxWateringTime)) {
+            if (!this->getState().waterState) 
+                this->turnOnWater(true);
+        } else {
+            if ((this->getState().timeWatering >= this->getState().maxWateringTime)) {
+                this->setTriggersEnabled(false); // Shut off triggers, something is wrong, the max watering time shoudln't be 
+            }                                    // hit if the sensors are working correctly
+            if (this->getState().waterState)
+                this->turnOnWater(false);
+        }
+    } else {
+        this->state.triggerHeld = 0;
+    }
+    if (this->getState().inOverride && this->getState().waterState) {
+        if (this->getState().timeWatering >= this->getState().maxOverrideTime) {
+            this->waterOverride(false); // end Override
+        }
+    }
 }
 
 void VVeggies::waterOverride(bool value) {
+    this->state.triggerHeld = 0;
+    this->state.inOverride = value;
     this->turnOnWater(value);
 }
 
@@ -83,7 +154,58 @@ void VVeggies::setUpdateFrequency(uint16_t freq){
     }
 }
 
+void VVeggies::setTriggerHoldTime(unsigned long trigHT) {
+    uint32_t val = EEPROM.readULong(EE_ADDR_TRIGHT);
+    if (val != trigHT) {
+        EEPROM.writeULong(EE_ADDR_TRIGHT, trigHT);
+        EEPROM.commit();
+        this->state.triggerHoldTime = trigHT;
+        this->_onStateUpdateCallback();
+    }
+}
+
+void VVeggies::setMaxWateringTime(unsigned long maxWT){
+    uint32_t val = EEPROM.readUShort(EE_ADDR_MAXWT);
+    if (val != maxWT) {
+        EEPROM.writeULong(EE_ADDR_MAXWT,maxWT);
+        EEPROM.commit();
+        this->state.maxWateringTime = maxWT;
+        this->_onStateUpdateCallback();
+    }
+}
+
+void VVeggies::setMaxOverrideTime(unsigned long maxOT){
+    uint32_t val = EEPROM.readUShort(EE_ADDR_MAXOT);
+    if (val != maxOT) {
+        EEPROM.writeULong(EE_ADDR_MAXOT,maxOT);
+        EEPROM.commit();
+        this->state.maxOverrideTime = maxOT;
+        this->_onStateUpdateCallback();
+    }
+}
+
+void VVeggies::setSoilTrigger(uint8_t number, uint16_t value) {
+    uint16_t val = number ? EEPROM.readUShort(EE_ADDR_ST1TRIG) : EEPROM.readUShort(EE_ADDR_ST0TRIG);
+    if (val != value) {
+        EEPROM.writeUShort(number ? EE_ADDR_ST1TRIG : EE_ADDR_ST0TRIG, value);
+        EEPROM.commit();
+        this->state.moistureSensorTrigger[number] = value;
+        this->_onStateUpdateCallback();
+    }
+}
+
 void VVeggies::updateInternalStateFromEEPROM() {
     this->state.updateFrequency = EEPROM.readUShort(EE_ADDR_UPDATEFREQ);
     Serial.println(String("Setting update frequency from EEPROM: ") + state.updateFrequency);
+    this->state.moistureSensorTrigger[0] = EEPROM.readUShort(EE_ADDR_ST0TRIG);
+    this->state.moistureSensorTrigger[1] = EEPROM.readUShort(EE_ADDR_ST1TRIG);
+    Serial.println(String("Set soil trigger 0,1 : ") + String(this->state.moistureSensorTrigger[0]) + String(",") +  this->state.moistureSensorTrigger[1]);
+    this->state.triggersEnabled = EEPROM.readBool(EE_ADDR_TRIGEN);
+    Serial.println(String("Set Triggers Enabled to: ") + this->state.triggersEnabled);
+    this->state.maxWateringTime = EEPROM.readULong(EE_ADDR_MAXWT);
+    Serial.println(String("Set Max Watering Time to: ") + this->state.maxWateringTime);
+    this->state.maxOverrideTime = EEPROM.readULong(EE_ADDR_MAXOT);
+    Serial.println(String("Set Max Override Time to: ") + this->state.maxOverrideTime);
+    this->state.triggerHoldTime = EEPROM.readULong(EE_ADDR_TRIGHT);
+    Serial.println(String("Set Trigger Hold Time to: ") + this->state.triggerHoldTime);
 }
